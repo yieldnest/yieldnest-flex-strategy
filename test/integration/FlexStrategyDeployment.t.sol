f5742127f2ce9a9ff1887f3388ef3ea009591494
ok_guy <5000825+iflp@users.noreply.github.com>
2025-05-27 11:33:47 +0800

    feat: added fork tests for upgrades and adding admin

--- test/integration/FlexStrategyDeployment.t.sol
+++ test/integration/FlexStrategyDeployment.t.sol
@@ -10,6 +10,10 @@ import { AccountingToken } from "src/AccountingToken.sol";
 import { IVault } from "@yieldnest-vault/interface/IVault.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { BaseScript } from "script/BaseScript.sol";
+import { UpgradeUtils } from "script/UpgradeUtils.sol";
+import { MainnetActors } from "@yieldnest-vault-script/Actors.sol";
+import { ProxyUtils } from "@yieldnest-vault-script/ProxyUtils.sol";
+import { RolesVerification } from "script/verification/RolesVerification.sol";
 
 contract FlexStrategyDeployment is Test {
     DeployFlexStrategy public deployment;
@@ -25,5 +29,29 @@ contract FlexStrategyDeployment is Test {
         VerifyFlexStrategy verify = new VerifyFlexStrategy();
         verify.setEnv(BaseScript.Env.TEST);
         verify.run();
+    }
+
+    function test_upgrade_success() public {
+        FlexStrategy newImpl = new FlexStrategy();
+        address securityCouncil = MainnetActors(address(deployment.actors())).YnSecurityCouncil();
+        UpgradeUtils.timelockUpgrade(
+            deployment.timelock(), securityCouncil, address(deployment.strategy()), address(newImpl)
+        );
+
+        assertEq(address(ProxyUtils.getImplementation(address(deployment.strategy()))), address(newImpl));
+    }
+
+    function test_addNewAdmin_success() public {
+        address newAdmin = address(0x1234567890123456789012345678901234567890);
+
+        vm.startPrank(deployment.actors().ADMIN());
+        deployment.strategy().grantRole(deployment.strategy().DEFAULT_ADMIN_ROLE(), newAdmin);
+        RolesVerification.verifyRole(
+            deployment.strategy(),
+            newAdmin,
+            deployment.strategy().DEFAULT_ADMIN_ROLE(),
+            true,
+            "newAdmin has DEFAULT_ADMIN_ROLE"
+        );
     }
 }
