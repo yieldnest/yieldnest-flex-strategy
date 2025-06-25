cc0dffeda0766b4c096756df304ff0d16d89887e
danoctavian <danoctavian91@gmail.com>
2025-06-25 04:14:14 +0300

    add flex strategy rules

--- script/rules/FlexStrategyRules.sol
+++ script/rules/FlexStrategyRules.sol
@@ -1,30 +1,28 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.24;
 
-import {IVault, IValidator} from "src/interface/IVault.sol";
-import {SafeRules} from "./SafeRules.sol";
+import { IVault, IValidator } from "src/interface/IVault.sol";
+import { SafeRules } from "./SafeRules.sol";
 
 library FlexStrategyRules {
-
-    function getDepositRule(address contractAddress)
-        internal
-        pure
-        returns (SafeRules.RuleParams memory)
-    {
+    function getDepositRule(address contractAddress) internal pure returns (SafeRules.RuleParams memory) {
         bytes4 funcSig = bytes4(keccak256("deposit(uint256)"));
 
         IVault.ParamRule[] memory paramRules = new IVault.ParamRule[](1);
 
         paramRules[0] =
-            IVault.ParamRule({paramType: IVault.ParamType.UINT256, isArray: false, allowList: new address[](0)});
+            IVault.ParamRule({ paramType: IVault.ParamType.UINT256, isArray: false, allowList: new address[](0) });
 
         IVault.FunctionRule memory rule =
-            IVault.FunctionRule({isActive: true, paramRules: paramRules, validator: IValidator(address(0))});
+            IVault.FunctionRule({ isActive: true, paramRules: paramRules, validator: IValidator(address(0)) });
 
-        return SafeRules.RuleParams({contractAddress: contractAddress, funcSig: funcSig, rule: rule});
+        return SafeRules.RuleParams({ contractAddress: contractAddress, funcSig: funcSig, rule: rule });
     }
 
-    function getWithdrawRule(address contractAddress, address receiver)
+    function getWithdrawRule(
+        address contractAddress,
+        address receiver
+    )
         internal
         pure
         returns (SafeRules.RuleParams memory)
@@ -34,16 +32,16 @@ library FlexStrategyRules {
         IVault.ParamRule[] memory paramRules = new IVault.ParamRule[](2);
 
         paramRules[0] =
-            IVault.ParamRule({paramType: IVault.ParamType.UINT256, isArray: false, allowList: new address[](0)});
+            IVault.ParamRule({ paramType: IVault.ParamType.UINT256, isArray: false, allowList: new address[](0) });
 
         address[] memory allowList = new address[](1);
         allowList[0] = address(receiver);
 
-        paramRules[1] = IVault.ParamRule({paramType: IVault.ParamType.ADDRESS, isArray: false, allowList: allowList});
+        paramRules[1] = IVault.ParamRule({ paramType: IVault.ParamType.ADDRESS, isArray: false, allowList: allowList });
 
         IVault.FunctionRule memory rule =
-            IVault.FunctionRule({isActive: true, paramRules: paramRules, validator: IValidator(address(0))});
+            IVault.FunctionRule({ isActive: true, paramRules: paramRules, validator: IValidator(address(0)) });
 
-        return SafeRules.RuleParams({contractAddress: contractAddress, funcSig: funcSig, rule: rule});
+        return SafeRules.RuleParams({ contractAddress: contractAddress, funcSig: funcSig, rule: rule });
     }
 }
