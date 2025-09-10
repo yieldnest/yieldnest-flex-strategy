cb0a7a2d9f2e6b84f4397d75476998c46ac1dc0f
danoctavian <danoctavian91@gmail.com>
2025-09-10 10:48:00 +0300

    add base mnainnet tests

---
+++ test/mainnet/upgrade.spec.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: BSD Clause-3
+pragma solidity ^0.8.24;
+
+import { Test } from "lib/forge-std/src/Test.sol";
+import { TimelockController } from "@openzeppelin/contracts/governance/TimelockController.sol";
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { IVault } from "@yieldnest-vault/interface/IVault.sol";
+import { FlexStrategy } from "src/FlexStrategy.sol";
+import { BaseMainnetTest } from "test/mainnet/BaseMainnetTest.sol";
+import { UpgradeUtils } from "script/UpgradeUtils.sol";
+import { DeployFlexStrategy } from "script/DeployFlexStrategy.s.sol";
+import { ProxyUtils } from "@yieldnest-vault-script/ProxyUtils.sol";
+
+contract VaultMainnetUpgradeTest is BaseMainnetTest {
+    function setUp() public override {
+        super.setUp();
+    }
+
+    function upgradeStrategy(
+        IVault vault,
+        address timelock,
+        address admin
+    )
+        internal
+        returns (FlexStrategy implementation)
+    {
+        {
+            implementation = new FlexStrategy();
+            UpgradeUtils.timelockUpgrade(
+                TimelockController(payable(timelock)), admin, address(vault), address(implementation)
+            );
+        }
+    }
+
+    function test_Vault_Upgrade_Implementation_Set_Correctly() public {
+        uint256 i = 0;
+        IVault vault = IVault(address(strategies[i]));
+        FlexStrategy implementation =
+            upgradeStrategy(vault, address(deployments[i].timelock()), deployments[i].actors().ADMIN());
+        // Verify the vault implementation was upgraded correctly
+        address currentVaultImpl = ProxyUtils.getImplementation(address(vault));
+        assertEq(currentVaultImpl, address(implementation), "Vault implementation not set correctly");
+    }
+}
